4fadde7190fe7b19fd439c511940ac27bb9d927f
vigneshfreshworks <vignesh.guru@freshworks.com>
2021-07-24 08:44:43 +0530

    test-commit

---
+++ main/java/com/qa/interview/Requests.java
@@ -0,0 +1,16 @@
+package com.qa.interview;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+public class Requests {
+	
+	
+
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
